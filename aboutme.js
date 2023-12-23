4f94248fd373b78a05945ae17676e115607116b8
Yubraj khatri <112848654+Yubraj977@users.noreply.github.com>
2023-12-22 22:54:32 -1000

    Create aboutme.js

---
+++ aboutme.js
@@ -0,0 +1,18 @@
+const sidbelbase = {
+  education: "UnderGraduate",
+  otherAlias: "Full Stack Developer",
+  codesIn: ["Javascript", "HTML", "CSS", "Python", "Bash"],
+  currentlylearning: ["MongoDB", "AI & ML","Django", "Scuva Diving"],
+  toolsUsing: ["Vue", "Flask", "Django", "FastAPI", "Sass", "AWS", "Wordpress", "Bootstrap", "Firebase", "Figma"],
+  experiences: [
+    {
+      company : "Behance",
+      post : "Graphic Designer | Freelancing"
+    },
+    {
+      company : "Eagle Eye Pvt. Ltd",
+      post : "Senior Graphic Designer"
+    }
+  ],
+availableForHire: true
+}
